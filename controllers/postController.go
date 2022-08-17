af068b09983e011685bd5eb020dfc6d51c655b0b
disco07 <koneenok@outlook.fr>
2022-08-17 23:11:47 +0200

    repository post

--- controllers/postController.go
+++ controllers/postController.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"encoding/json"
+	"github.com/disco07/blog/models"
 	"github.com/disco07/blog/utils"
 	"github.com/gorilla/mux"
 	"net/http"
@@ -26,4 +28,12 @@ func (s Server) getAllPosts(w http.ResponseWriter, _ *http.Request) {
 		utils.Error(w, http.StatusBadRequest, err)
 	}
 	utils.JSON(w, http.StatusOK, posts)
+}
+
+func (s Server) createPost(w http.ResponseWriter, r *http.Request) {
+	var post models.Post
+	if err := json.NewDecoder(r.Body).Decode(&post); err != nil {
+		utils.Error(w, http.StatusBadRequest, err)
+		return
+	}
 }
--- repository/postRepository.go
+++ repository/postRepository.go
@@ -32,3 +32,16 @@ func (r Repository) FindAllPost() ([]*models.Post, error) {
 
 	return posts, nil
 }
+
+func (r Repository) InsertMovie(post models.Post) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := `INSERT INTO post (author_id, title, slug, summary, content, published_at) VALUES ($1, $2, $3, $4, $5, $6)`
+	_, err := r.DB.NamedExecContext(ctx, query, post)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
