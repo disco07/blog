370a73b8a4b80e28cd8d676b64f38882d7431fd6
disco07 <koneenok@outlook.fr>
2022-08-19 23:17:01 +0200

    creation des routes

--- controllers/routes.go
+++ controllers/routes.go
@@ -14,5 +14,11 @@ func (s Server) initializeRoutes() http.Handler {
 	r.HandleFunc("/post/{id:[0-9]+}", s.updatePost).Methods(http.MethodPut)
 	r.HandleFunc("/post/{id:[0-9]+}", s.deletePost).Methods(http.MethodDelete)
 
+	r.HandleFunc("/tag/{id:[0-9]+}", s.getOneTag).Methods(http.MethodGet)
+	r.HandleFunc("/tags/", s.getAllTags).Methods(http.MethodGet)
+	r.HandleFunc("/tags/", s.createTag).Methods(http.MethodPost)
+	r.HandleFunc("/tag/{id:[0-9]+}", s.updateTag).Methods(http.MethodPut)
+	r.HandleFunc("/tag/{id:[0-9]+}", s.deleteTag).Methods(http.MethodDelete)
+
 	return r
 }
