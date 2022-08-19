76ccdd614f544ca999f094cc7045d73482b17bee
disco07 <koneenok@outlook.fr>
2022-08-19 23:15:27 +0200

    creation des routes

--- controllers/routes.go
+++ controllers/routes.go
@@ -10,6 +10,9 @@ func (s Server) initializeRoutes() http.Handler {
 	r.HandleFunc("/", s.home).Methods(http.MethodGet)
 	r.HandleFunc("/post/{id:[0-9]+}", s.getOnePost).Methods(http.MethodGet)
 	r.HandleFunc("/posts/", s.getAllPosts).Methods(http.MethodGet)
+	r.HandleFunc("/posts/", s.createPost).Methods(http.MethodPost)
+	r.HandleFunc("/post/{id:[0-9]+}", s.updatePost).Methods(http.MethodPut)
+	r.HandleFunc("/post/{id:[0-9]+}", s.deletePost).Methods(http.MethodDelete)
 
 	return r
 }
