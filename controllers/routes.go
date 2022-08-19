430a6e061b57c45ef9454a3738d2240fdd4a0a2f
disco07 <koneenok@outlook.fr>
2022-08-19 23:17:41 +0200

    creation des routes

--- controllers/routes.go
+++ controllers/routes.go
@@ -8,12 +8,15 @@ import (
 func (s Server) initializeRoutes() http.Handler {
 	r := mux.NewRouter()
 	r.HandleFunc("/", s.home).Methods(http.MethodGet)
+
+	// Routes pour les posts
 	r.HandleFunc("/post/{id:[0-9]+}", s.getOnePost).Methods(http.MethodGet)
 	r.HandleFunc("/posts/", s.getAllPosts).Methods(http.MethodGet)
 	r.HandleFunc("/posts/", s.createPost).Methods(http.MethodPost)
 	r.HandleFunc("/post/{id:[0-9]+}", s.updatePost).Methods(http.MethodPut)
 	r.HandleFunc("/post/{id:[0-9]+}", s.deletePost).Methods(http.MethodDelete)
 
+	// Routes pour les tags
 	r.HandleFunc("/tag/{id:[0-9]+}", s.getOneTag).Methods(http.MethodGet)
 	r.HandleFunc("/tags/", s.getAllTags).Methods(http.MethodGet)
 	r.HandleFunc("/tags/", s.createTag).Methods(http.MethodPost)
