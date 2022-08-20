a20ee29c7dc2f7fd06d14f690b56d669fff75f4a
disco07 <koneenok@outlook.fr>
2022-08-20 17:08:14 +0200

    ajout de la methode creation de user

--- repository/userRepository.go
+++ repository/userRepository.go
@@ -1,1 +1,27 @@
 package repository
+
+import (
+	"context"
+	"github.com/disco07/blog/models"
+	"time"
+)
+
+func (r Repository) CreateUser(user models.User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := `INSERT INTO user (full_name, username, email, password) VALUES ($1, $2, $3, $4)`
+
+	stmt, err := r.DB.PrepareContext(ctx, query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = r.DB.NamedExecContext(ctx, query, user)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
