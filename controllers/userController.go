c9fc1b2404ea538cdddf96e507659d51ff4f5e52
disco07 <koneenok@outlook.fr>
2022-08-20 16:59:55 +0200

    creation du user controller

---
+++ controllers/userController.go
@@ -0,0 +1,11 @@
+package controllers
+
+import "net/http"
+
+func (s Server) createUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s Server) login(w http.ResponseWriter, r *http.Request) {
+
+}
