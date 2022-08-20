eefd134cdfff6068079d25f23c9cf8b155d7ddb4
disco07 <koneenok@outlook.fr>
2022-08-20 21:39:47 +0200

    creation de user

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,9 +1,35 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/disco07/blog/models"
+	"github.com/disco07/blog/utils"
+	"net/http"
+)
 
 func (s Server) createUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
 
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		utils.Error(w, http.StatusBadRequest, err)
+		return
+	}
+	password, err := utils.HashPassword(user.Password)
+	if err != nil {
+		utils.Error(w, http.StatusBadRequest, err)
+		return
+	}
+	user.Password = password
+
+	if err := s.repo.CreateUser(user); err != nil {
+		utils.Error(w, http.StatusBadRequest, err)
+		return
+	}
+	res := jsonResponse{
+		Status:  true,
+		Message: "user created successfully",
+	}
+	utils.JSON(w, http.StatusCreated, res)
 }
 
 func (s Server) login(w http.ResponseWriter, r *http.Request) {
